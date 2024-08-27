b709594e3e5564c5c50efb788519ab3a6cc56024
Bimal Kunwar <bimalknwr3@gmail.com>
2024-08-27 11:37:10 +0545

    Update accept_email_and_chk_@.cpp

--- Sixth Lab/accept_email_and_chk_@.cpp
+++ Sixth Lab/accept_email_and_chk_@.cpp
@@ -4,13 +4,20 @@
 using namespace std;
 
 int main() {
-    for (int i = 0; i < 3; i++) { 
+    for (int i = 0; i < 2; i++) { 
         try {
             string email;
             cout << "Enter Email Id : ";
             cin >> email;
-
-            if (email.find('@') == string::npos || email.find('.') == string::npos) { 
+            bool emailvalid = false;
+            for(int i = 0; i<email.length() ; i++){
+                
+            if (email[i] == '@') { 
+                emailvalid = true;
+                break;
+            }
+            }
+            if(!emailvalid){
                 throw runtime_error("Invalid Email Id!!!");
             } 
 

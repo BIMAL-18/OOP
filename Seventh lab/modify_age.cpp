11b5594200a87399187063ae64216d8b04c1d453
Bimal Kunwar <bimalknwr3@gmail.com>
2024-09-18 22:08:58 +0545

    Update modify_age.cpp

--- Seventh lab/modify_age.cpp
+++ Seventh lab/modify_age.cpp
@@ -5,11 +5,10 @@
 using namespace std;
 int main() {
     ifstream inputFile("student_records.txt");
-    ofstream tempFile("temp_records.txt");
+    ofstream tempFile("temp.txt");
     string line;
     if (!inputFile) {
         cerr << "Error opening student_records.txt." << endl;
-        return 1; 
     }
 
     while (getline(inputFile, line)) {
@@ -22,7 +21,7 @@ int main() {
     inputFile.close(); 
     tempFile.close(); 
     remove("student_records.txt");
-    rename("temp_records.txt", "student_records.txt");
+    rename("temp.txt", "student_records.txt");
     cout << "Age of Shyam has been modified to 22." << endl;
 
     return 0; 

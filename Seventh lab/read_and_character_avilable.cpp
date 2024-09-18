c8419ea0407723ff6a1a2ab57acf856f65d993a3
Bimal Kunwar <bimalknwr3@gmail.com>
2024-09-18 22:23:14 +0545

    Update read_and_character_avilable.cpp

--- Seventh lab/read_and_character_avilable.cpp
+++ Seventh lab/read_and_character_avilable.cpp
@@ -9,15 +9,14 @@ int main() {
   ifstream inputFile("demo.txt");
   if (!inputFile.is_open()) {
     cerr << "Error opening file demo.txt" << endl;
-    return 1; 
   }
-  int charCount = 0;
+  int charcount = 0;
   char ch;
   while (inputFile.get(ch)) {
-    charCount++;
+    charcount++;
   }
   inputFile.close();
-  cout << "Number of characters in the file : " << charCount << endl;
+  cout << "Number of characters in the file : " << charcount << endl;
 
   return 0;
 }
